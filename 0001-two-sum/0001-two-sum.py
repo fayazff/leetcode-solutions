fe54ad5e68f95358ca9890bdf4aae15bbc08edf6
fayazff <127029354+fayazff@users.noreply.github.com>
2026-04-14 11:56:11 +0530

    Time: 3 ms (52.87%), Space: 20.5 MB (25.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       n=len(nums)
-       for i in range(n-1):
-        for j in range(i+1,n):
-            if nums[i]+nums[j]== target:
-                return[i,j]
-       return[] #No solution found         
+        hashmp={}
+        for i in range(len(nums)):
+            compare=target - nums[i]
+            if compare in hashmp:
+                return [hashmp[compare],i]
+            hashmp[nums[i]]=i
+            
